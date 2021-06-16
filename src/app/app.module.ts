13cb7542de59f6617c2bd547e214e3014341d6c4
Daniel Jan Szczebiot <szczebiot.daniel@gmail.com>
2021-06-16 15:32:36 +0200

    Navbartest and color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,14 @@ import { ClassesComponent } from './components/classes/classes.component';
 import { QuestComponent } from './components/quest/quest.component';
 import { ButtonComponent } from './components/button/button.component';
 import { MapComponent } from './components/map/map.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeroMatNavigationComponent } from './components/hero-mat-navigation/hero-mat-navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
@@ -17,11 +26,20 @@ import { MapComponent } from './components/map/map.component';
     ClassesComponent,
     QuestComponent,
     ButtonComponent,
-    MapComponent
+    MapComponent,
+    HeroMatNavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/hero-mat-navigation/hero-mat-navigation.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-hero-mat-navigation',
+  templateUrl: './hero-mat-navigation.component.html',
+  styleUrls: ['./hero-mat-navigation.component.css']
+})
+export class HeroMatNavigationComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
